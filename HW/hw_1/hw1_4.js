d3d69b2236647d763cf51cea3784c270d5067c9d
Andy Candy <andrey.tsiluyko@gmail.com>
2024-02-23 00:28:27 +0200

    1.1 1.2 1.3 hw done

---
+++ HW/hw_1/hw1_4.js
@@ -0,0 +1,6 @@
+let name = prompt('Enter your name');
+console.log(name);
+let lastName = prompt('Enter your Last name');
+console.log(lastName);
+let age = +prompt('Enter your Age');
+console.log(age);
