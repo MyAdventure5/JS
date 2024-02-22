b5f479d72d412303c5d7eb22d977f03702125f92
Andy Candy <andrey.tsiluyko@gmail.com>
2024-02-23 00:14:07 +0200

    1.1 1.2 1.3 hw done

---
+++ HW/hw_1/hw1_2.js
@@ -0,0 +1,6 @@
+// firstName, middleName, lastName,
+let firstName = "Andrey";
+let middleName = 'Victorovich';
+let lastName = 'Tsiluiko';
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person)
