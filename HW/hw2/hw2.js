7c139fe53bd0518307824395ab523cb14bfa6a9a
Andy Candy <andrey.tsiluyko@gmail.com>
2024-02-26 00:15:31 +0200

    hw2 task: 1, 2, 3 done

--- HW/hw2/hw2.js
+++ HW/hw2/hw2.js
@@ -32,7 +32,7 @@ console.log(phoneList[0].price)
 console.log(phoneList[0].currency)
 console.log(phoneList[0].availability)
 
-console.log('_______________task1_1_1_______________')
+console.log('_______________else_______________')
 
 let list = [
     395460636,
@@ -61,65 +61,131 @@ console.log(list[9])
 
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
 
-console.log('_______________task1_2_______________')
-let bookOne = {
-    title: 'Witcher',
-    pageCount: 255,
-    genre: 'fantastic',
-}
-let bookTwo = {
-    title: 'The Adventure of Tom Sawyer',
-    pageCount: 168,
-    genre: 'Novel',
-}
-let bookThree = {
-    title: 'The Art of War',
-    pageCount: 68,
-    genre: 'treatise',
-}
+    console.log('_______________task1_2_______________')
+    let bookOne = {
+        title: 'Witcher',
+        pageCount: 255,
+        genre: 'fantastic',
+    }
+    let bookTwo = {
+        title: 'The Adventure of Tom Sawyer',
+        pageCount: 168,
+        genre: 'Novel',
+    }
+    let bookThree = {
+        title: 'The Art of War',
+        pageCount: 68,
+        genre: 'treatise',
+    }
 console.log(bookOne, bookTwo, bookThree)
 
 // Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - являється  масивом. Кожен автор має поля name та age.
 
 console.log('_______________task1_3_______________')
-let authorsFirst = [
-    {
-        name: 'Andrzej Sapkowski',
-        age: 75
+
+    let authorsFirst = [
+        {
+            name: 'Andrzej Sapkowski',
+            age: 75
+        }
+    ]
+    let authorsSecond = [
+        {
+            name: 'Mark Twain',
+            age: 74
+        }
+    ]
+    let authorsThird = [
+        {
+            name: 'Sun Tzu',
+            age: 48
+        }
+    ]
+    let bookFirst = {
+        title: 'Witcher',
+        pageCount: 255,
+        genre: 'fantastic',
+        authorsFirst
     }
-]
-let authorsSecond = [
-    {
-        name: 'Mark Twain',
-        age: 74
+    let bookSecon = {
+        title: 'The Adventure of Tom Sawyer',
+        pageCount: 168,
+        genre: 'Novel',
+        authorsSecond
     }
-]
-let authorsThird = [
-    {
-        name: 'Sun Tzu',
-        age: 48
+    let bookThird = {
+        title: 'The Art of War',
+        pageCount: 68,
+        genre: 'treatise',
+        authorsThird
     }
-]
-let bookFirst = {
-    title: 'Witcher',
-    pageCount: 255,
-    genre: 'fantastic',
-    authorsFirst
-}
-let bookSecon = {
-    title: 'The Adventure of Tom Sawyer',
-    pageCount: 168,
-    genre: 'Novel',
-    authorsSecond
-}
-let bookThird = {
-    title: 'The Art of War',
-    pageCount: 68,
-    genre: 'treatise',
-    authorsThird
-}
-console.log(bookFirst, bookSecon, bookThird)
+    console.log(bookFirst, bookSecon, bookThird)
 
 // Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
 
 console.log('_______________task1_4_______________')
+
+
+let userArr = [
+    {name: 'Andrey', username: 'Andy', password: 'EHcckx28'},
+    {name: 'Sergey', username: 'GreySer', password: 'Q6Cokp2q'},
+    {name: 'Matvey', username: 'MataM', password: 'KNi6wG6p'},
+    {name: 'Alex', username: 'Lux', password: '0n7Wmc13'},
+    {name: 'Gurgen', username: 'Argonium', password: '8vNB8ZKj'},
+    {name: 'Albert', username: 'NotEinstein', password: 'TMcGejnt'},
+    {name: 'Micha', username: 'Matcha', password: 'uCuodNEW'},
+    {name: 'Mark', username: '2turbo', password: 'uxFEZQnx'},
+    {name: 'Lion', username: 'Kitty', password: 'AfzDJS9A'},
+    {name: 'Adler', username: 'HellDiver', password: 'R0uUPTGd'},
+    {name: 'Conor', username: 'IBB', password: 'U%~HgSy[mLN#Kg2<8QDo'}
+];
+    console.log(userArr[0].password)
+    console.log(userArr[1].password)
+    console.log(userArr[2].password)
+    console.log(userArr[3].password)
+    console.log(userArr[4].password)
+    console.log(userArr[5].password)
+    console.log(userArr[6].password)
+    console.log(userArr[7].password)
+    console.log(userArr[8].password)
+    console.log(userArr[9].password)
+
+// Логічні розгалуження:
+// - Є змінна х, якій ви надаєте довільне числове значення.
+//  Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+// - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
+// (в першу, другу, третю или четверту частину години).
+// - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+// - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
+// - Користувач вводить або має два числа.
+//    Потрібно знайти та вивести максимальне число з тих двох .
+//    Також потрібно врахувати коли введені рівні числа.
+
+console.log('_______________task2_1_______________')
+
+    let x  = +prompt('enter number from -3 to 3');
+    if (x !== 0 ){
+        document.write('<h1>Вірно</h1>')
+    }
+    else {
+        document.write('<h1>Хиба</h1>')
+    }
+
+// - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
+// // (в першу, другу, третю или четверту частину години).
+
+console.log('_______________task2_2_______________')
+
+
+    if (a => 0 && a < 15 ){
+        document.write('<h1> 1 quoter</h1>');
+    }
+    else if (time => 15 && time < 30 ){
+        document.write('<h1>2 quoter</h1>');
+    }
+    else if (time => 30 && time < 45){
+        document.write('<h1>3 quoter</h1>');
+    }
+    else if (time => 45 && time <= 59 ){
+        document.write('<h1>4 quoter</h1>');
+    }
