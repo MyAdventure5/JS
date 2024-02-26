29c7813f40139f89f1210d78da948a345f819c36
Andy Candy <andrey.tsiluyko@gmail.com>
2024-02-27 01:34:52 +0200

    hw2 done

--- HW/hw2/hw2.js
+++ HW/hw2/hw2.js
@@ -280,6 +280,8 @@ console.log('_______________task2_6_______________')
 
 
 
+    let b = undefined
+ console.log(b || b || 'default')
 
 
 
@@ -287,4 +289,33 @@ console.log('_______________task2_6_______________')
 
 console.log('_______________task2_7_______________')
 
-// - з файлу arrays.js (лежить000 в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+// - з файлу arrays.js (лежить000 в папці 2023 plan ) взяти масив coursesAndDurationArray.
+// За допомоги іф перевірити кожен його елемент на тривалість навчання.
+// У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+if (coursesAndDurationArray[0].monthDuration > 5) {
+    console.log(coursesAndDurationArray[0].title + ' - Супер');
+}
+if (coursesAndDurationArray[1].monthDuration > 5) {
+    console.log(coursesAndDurationArray[1].title + ' - Супер');
+}
+if (coursesAndDurationArray[2].monthDuration > 5) {
+    console.log(coursesAndDurationArray[2].title + ' - Супер');
+}
+if (coursesAndDurationArray[3].monthDuration > 5) {
+    console.log(coursesAndDurationArray[3].title + ' - Супер');
+}
+if (coursesAndDurationArray[4].monthDuration > 5) {
+    console.log(coursesAndDurationArray[4].title + ' - Супер');
+}
+if (coursesAndDurationArray[5].monthDuration > 5) {
+    console.log(coursesAndDurationArray[5].title + ' - Супер');
+}
